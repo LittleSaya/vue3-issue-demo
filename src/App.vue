0dc32352383e008f11f0071067a5ffab5b1fcc13
LittleSaya <2472609815@qq.com>
2023-07-12 19:04:30 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Comp :a="[1, 2]" :b="b" :c="-1" />
+  <button @click="fn">fn</button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Comp from './Comp.vue';
+
+const b = ref<number[]>([]);
+
+function fn() {
+  b.value = [3, 4];
+}
+</script>
---
+++ src/Comp.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>nothing here</span>
+</template>
+
+<script setup lang="ts">
+import { watch } from 'vue';
+
+const props = defineProps<{
+  a: number[];
+  b: number[];
+  c: number;
+}>();
+
+watch(
+  () => props.c,
+  () => {
+    alert('dbg watch c');
+  },
+);
+
+watch(
+  () => props.b,
+  () => {
+    alert('dbg watch b');
+  },
+);
+
+watch(
+  () => props.a,
+  () => {
+    alert('dbg watch a');
+  },
+);
+</script>
